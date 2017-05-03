891170e0a733deb2a4f5677690fea268d6cf9fc3
5jlin <a2rriadai@yahoo.com.tw>
2017-05-04 02:00:05 +0800

    top response
    top3response done

--- getpttresponse/gettopresponse.py
+++ getpttresponse/gettopresponse.py
@@ -12,27 +12,23 @@ def list2freqdict(mylist):
         mydict[ch]=mydict.get(ch,0)+1
     return mydict
 
+#handle stopword
 stopword = []
-
 pttstop = readfile.read('ptt_words.txt')
 speaial = readfile.read('specialMarks.txt')
 chinesestop = readfile.read('chinese_sw.txt')
-
 stopword = pttstop + speaial + chinesestop
 
-
-#pprint(data)
 num = len(data["articles"])
-response = []
 #for i in range(num):
-for i in range(6,7):
+for i in range(num):
+    response = []
     datatmp = data["articles"][i]
-    print(i)
-    print(data["articles"][i]["article_title"])
-    print(data["articles"][i]["content"])
-    tmp = []
+    #print(i,data["articles"][i]["article_title"])
+    title = data["articles"][i]["article_title"]
+    #print(data["articles"][i]["content"])
     s = ""
-    if "messages" in datatmp:
+    if "messages" in datatmp and data["articles"][i]["article_title"] != "" and data["articles"][i]["content"] != "":
         messages = data["articles"][i]["messages"]
         j = 0
         while j < len(messages):
@@ -50,35 +46,37 @@ for i in range(6,7):
                     #print(s)
                     response.append(s)
                     j = j+z+1
-                    break
-     
-    
-    else:continue
+                    break     
+#    else:continue
 
-allword = []
-for i in response :
-    words = jieba.cut(i, cut_all=False)
-    for word in words:
-        allword.append(word)
+        allword = []
+        for i in response :
+            words = jieba.cut(i, cut_all=False)
+            for word in words:
+                allword.append(word)
+                
+        d = {}
+        for i in allword :
+            if i in d:
+                d[i] += 1
+            else:
+                d[i] = 1
+          
+        scorelist = []
+        answerlist = []
+        for i in response :
+            score = 0
+            words = jieba.cut(i, cut_all=False)
+            for word in words:
+                if word not in stopword:
+                    score += d[word]
+            scorelist.append(score)
+            answerlist.append(i)
+            #print(i,score)
         
-d = {}
-for i in allword :
-    if i in d:
-        d[i] += 1
-    else:
-        d[i] = 1
+        if len(sorted(zip(scorelist, answerlist), reverse=True)) > 3:
+            print(title)
+            print(sorted(zip(scorelist, answerlist), reverse=True)[:3][0][1])
+            print(sorted(zip(scorelist, answerlist), reverse=True)[:3][1][1])
+            print(sorted(zip(scorelist, answerlist), reverse=True)[:3][2][1])
 
-    
-
-
-for i in response :
-    score = 0
-    tmp = []
-    words = jieba.cut(i, cut_all=False)
-    for word in words:
-        #tmp.append(word)
-        if word not in stopword:
-            score += d[word]
-    print(i,score)
-
-    
