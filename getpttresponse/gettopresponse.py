bc03d20ec6de80f057416bea10900561a0c94ebb
5jlin <a2rriadai@yahoo.com.tw>
2017-05-10 02:41:08 +0800

    summarize content

--- getpttresponse/gettopresponse.py
+++ getpttresponse/gettopresponse.py
@@ -2,8 +2,10 @@ import json
 import jieba
 from operator import itemgetter
 import readfile
+import textrank
+import math
 
-with open('Boy-Girl-3356-3358.json', encoding = 'utf-8-sig') as data_file:    
+with open('Boy-Girl-3424-3424.json', encoding = 'utf-8-sig') as data_file:    
     data = json.load(data_file, encoding = 'utf-8-sig')
 
 def list2freqdict(mylist):
@@ -23,10 +25,20 @@ num = len(data["articles"])
 #for i in range(num):
 for i in range(num):
     response = []
+    contentmain = []
     datatmp = data["articles"][i]
     #print(i,data["articles"][i]["article_title"])
     title = data["articles"][i]["article_title"]
+    content = data["articles"][i]["content"]
+    #print('內文')
     #print(data["articles"][i]["content"])
+    #print('摘要')
+    textranktmp = textrank.textrank4zh(content)
+    #print(textranktmp)
+    for m in content.split('\n'):
+        if m in textranktmp:
+            contentmain.append(m)
+    
     s = ""
     if "messages" in datatmp and data["articles"][i]["article_title"] != "" and data["articles"][i]["content"] != "":
         messages = data["articles"][i]["messages"]
@@ -64,19 +76,25 @@ for i in range(num):
           
         scorelist = []
         answerlist = []
+        countword = 0
         for i in response :
             score = 0
             words = jieba.cut(i, cut_all=False)
             for word in words:
                 if word not in stopword:
                     score += d[word]
-            scorelist.append(score)
-            answerlist.append(i)
+                    countword += 1
+            if countword > 1:
+                score = score/math.log(countword)
+                scorelist.append(score)
+                answerlist.append(i)
             #print(i,score)
-        
+    
+    
         if len(sorted(zip(scorelist, answerlist), reverse=True)) > 3:
             print(title)
+            print(contentmain)
             print(sorted(zip(scorelist, answerlist), reverse=True)[:3][0][1])
             print(sorted(zip(scorelist, answerlist), reverse=True)[:3][1][1])
             print(sorted(zip(scorelist, answerlist), reverse=True)[:3][2][1])
-
+        
